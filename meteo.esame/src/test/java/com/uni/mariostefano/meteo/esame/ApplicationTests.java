124fa2f6aaee4f6dbdf320af156560ad4c527b06
s1098199 <s1098199@studenti.univpm.it>
2021-12-13 13:05:41 +0100

    .

---
+++ meteo.esame/src/test/java/com/uni/mariostefano/meteo/esame/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.mariostefano.meteo.esame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
