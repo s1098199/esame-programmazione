1755b719b9855c12e50a952582bc909bfb997fdc
mariodeberardinis <S1098952@studenti.univpm.it>
2021-12-14 11:54:58 +0100

    Implementazione Service Implement

--- meteo.esame/src/main/java/com/uni/mariostefano/meteo/esame/controller/ServiceImpl.java
+++ meteo.esame/src/main/java/com/uni/mariostefano/meteo/esame/controller/ServiceImpl.java
@@ -1,5 +1,22 @@
 package com.uni.mariostefano.meteo.esame.controller;
 
-public class ServiceImpl {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.net.*;
+import java.io.*;
+
+@Service
+public class ServiceImp Implements Service {
+
+//string apiKey indica la chiave con la quale ricavare le informazioni da OpenWeather
+private string apiKey = "67e3d274028b01442141ddff3600d722";
+
+//metodo privato dell'URL
+private string url = http://api.openweathermap.org/data/2.5/weather?q=
+
 
 }
