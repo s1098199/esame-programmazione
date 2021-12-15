9e2b3e44ea811fe1bf0254f92345de37b187a149
s1098199 <s1098199@studenti.univpm.it>
2021-12-15 10:11:45 +0100

    modifica classe city

--- meteo.esame/src/main/java/com/uni/mariostefano/meteo/esame/model/City.java
+++ meteo.esame/src/main/java/com/uni/mariostefano/meteo/esame/model/City.java
@@ -13,14 +13,18 @@ import java.util.Vector;
 		private Vector<forecasts> vector = new Vector<forecasts>();
 		
 
-	//costruttore dell'Id della città	
+	/**Costruttore dell'Id della città
+	* @param id        ID della città
+	*/
 		public City(long id) {
 			this.id=id;
 			this.name=null;
 			this.coordinates=null;
 			this.Nation=null;
 		}
-		//costruttore del nome della città	
+		/** Costruttore del nome della città
+	     * @param name      nome della città
+	     */	
 		public City(String name) {
 			this.id=0;
 			this.name=name;
@@ -28,7 +32,9 @@ import java.util.Vector;
 			this.Nation=null;
 		}
 		
-		//costruttore delle coordinate della città	
+		/**Costruttore delle  coordinate della città	
+		 * @param coordinates coordinate della città
+		 */
 		public City(Coordinates coordinates) {
 			this.id=0;
 			this.name=null;
@@ -36,8 +42,10 @@ import java.util.Vector;
 			this.Nation=null;		
 		}
 		
-		//costruttore del nome della città	
-		//costruttore dello Stato della città	
+		/**Costruttore	
+		 * @param name     nome della città
+		 * @param Nation nazione dela città
+		 */
 		public City(String name, String Nation) {
 			this.id=0;
 			this.name=name;
@@ -45,43 +53,110 @@ import java.util.Vector;
 			this.Nation=Nation;
 		}
 		
-		//costruttore completo
-		//costruttore del nome della città
-		//costruttore della nazione della città
-		//costruttore dell'id della città
-		//costruttore delle coordinate della città
-		//costruttore deò vettore delle previsioni ristrette
-		
-		public City(long id, String name, Coordinates coordinates, String Nation) {
+		/**Costruttore completo
+		 * @param name         nome della città
+		 * @param nation       nazione della città
+		 * @param id           id della città
+		 * @param coordinates  coordinate della città
+		 * @param vector       vettore delle previsioni ristrette
+		*/
+		public City(long id, String name, Coordinates coordinates, String Nation, double 
+				pressure, float humidity, int temperature) {
 			this.id = id;
 			this.name = name;
 			this.coordinates = coordinates;
 			this.Nation = Nation;
-			this.vector = vector;
+			//this.vector = vector;
 		}
 	
-	//metodo che restituisce il nome	
+	/**
+	 * Metodo che restituisce il nome	
+	 * @return name
+	 */
 		public String getName() {
 			return name;
 		}
 		
-		//metodo che setta il nome
+		/**
+		 * Metodo che setta il nome
+		 * @param  String name
+		 */
 		public void setName(String name) {
 			this.name = name;
 		}
 		
-		//metodo che restituisce la nazione
+		/**
+		 * Metodo che restituisce la Nazione
+		 * @return Nation
+		 */
 		public String getNation() {
 			return Nation;
 		}
 		
-		//metodo che setta la nazione
+		/**
+		 * Metodo che setta la Nazione
+		 * @param String Nation
+		 */
 		public void setNation(String Nation) {
 			this.Nation = Nation;
 		}
 		
-		//metodo che restituisce l'Id
-		public long getId() {
-			return id;
+		/**
+		 * Metodo che setta l'Id
+		 * @param long id
+		 */
+		 	public void setId(long id) {
+			this.id = id;
 		}
+		/**
+		 * Metodo che restitutisce l'Id
+		 * @return Id
+		 */
+		 public long getId() {
+		 return id;
+		}
+		 /**
+		  * Metodo che restituisce le Coordinate
+		  * @return coordinates
+		  */
+		public Coordinates getCoordinates() {
+			return coordinates;
+		}
+		
+		/**
+		 * Metodo che setta le Coordinate
+		 * @param coordinates
+		 */
+		public void setCoordinates(Coordinates coordinates) {
+			this.coordinates = coordinates;
+		}
+		
+		/**
+		 * Metodo che restituisce il vettore delle previsioni della città
+		 * @return vector
+		 */
+		public Vector<forecasts> getVector() {
+			return vector;
+		}
+		
+		/**
+		 * Metodo che setta il vettore delle previsioni della città
+		 * @param vector di forecasts
+		 */
+		public void setVector(Vector<forecasts> vector) {
+			this.vector = vector;
+		}
+		
+		/**
+		 * Override del metodo toString
+		 * @return String che rappresenta la città
+		 */
+		
+		@Override
+		public String toString() {
+		 return "id=" + id + ", name=" + name + ", coordinates=" + coordinates + ", Nation=" + Nation
+					+ ", forecastsArray=" + toString() + ""; 
+		}
+		
+		
 }
